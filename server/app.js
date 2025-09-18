7e3d096d6f51f6b9dbe6a98f7f0b7fc97d2e5f14
ansari katib <ansarikatib647@gmail.com>
2025-09-19 00:51:18 +0530

    .

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin:process.env.FRONTEND_URL, 
+  origin:"*", 
   credentials: true,
   methods: ["GET","POST","PUT","DELETE","OPTIONS"],
 
